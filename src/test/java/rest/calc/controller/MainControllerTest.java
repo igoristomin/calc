04884b1afbd72f269e4fed4a85f5a14e0be7d73c
Igor Istomin <igoristomin.lic@yandex.ru>
2022-04-20 14:48:50 +0300

    test: create MainControllerTest

---
+++ src/test/java/rest/calc/controller/MainControllerTest.java
@@ -0,0 +1,40 @@
+package rest.calc.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainControllerTest {
+
+    private MainController mainController;
+
+    @BeforeEach
+    void setUp() {
+        mainController = new MainController();
+    }
+
+    // Add
+    @Test
+    void testAdd() {
+        assertEquals(mainController.showIndex("add", 3, 2), "5");
+    }
+
+    // Subtract
+    @Test
+    void testSub() {
+        assertEquals(mainController.showIndex("sub", 3, 2), "1");
+    }
+
+    // Multiply
+    @Test
+    void testMult() {
+        assertEquals(mainController.showIndex("mult", 3, 2), "6");
+    }
+
+    // Divide
+    @Test
+    void testDiv() {
+        assertEquals(mainController.showIndex("div", 3, 2), "1,5");
+    }
+
+}
